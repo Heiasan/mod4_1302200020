814f6b33a02c0f8d46115d7dbabeab1a78ae3d19
Heiasan <abdillahhasan130@gmail.com>
2022-03-19 17:50:50 +0700

    Kodingan Penuh Abdillah Hasan

--- modul4_1302200020/modul4_1302200020/Program.cs
+++ modul4_1302200020/modul4_1302200020/Program.cs
@@ -1,11 +1,18 @@
-﻿namespace modul4_1302200020
+﻿using modul4_13022200020;
+
+namespace modul4_1302200020
 {
-     public class MainClass
+    public class MainClass
     {
         public static void Main(string[] args)
         {
-            // NIM = 1302200020
+
             Penjumlahan.JumlahTigaAngka<long>(13, 02, 20);
+            SimpleDataBase<long> objData1 = new SimpleDataBase<long>();
+            objData1.AddNewData(13);
+            objData1.AddNewData(02);
+            objData1.AddNewData(20);
+            objData1.PrintAllData();
         }
     }
 }
